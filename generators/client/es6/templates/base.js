f6fc62dfc6deed25c5e8c29169e1b69577bb8adb
Ryan Bennett <ryan@filament.com>
2018-07-24 23:40:48 -0600

    Update base.js

--- generators/client/es6/templates/base.js
+++ generators/client/es6/templates/base.js
@@ -1,41 +1,49 @@
 class RPCClient {
-
-  constructor(url) {
+  constructor(url, username, password) {
+    this.username = username
+    this.password = password
     this.url = url;
   }
 
   request(method, params) {
-
     return new Promise((resolve, reject) => {
-
+      let headers
+      if (this.username && this.password) {
+        headers = new Headers();
+        headers.append('Authorization', 'Basic ' + Buffer.from(this.username + ":" + this.password).toString('base64'));
+      }
+      
       fetch(this.url, {
-          method: 'post',
-          body: JSON.stringify({
-            jsonrpc: "2.0",
-            id: Math.random().toString(16).slice(2),
-            method: method,
-            params: params
-          }),
-        }).then((response) => {
+        method: "post",
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          id: Math.random()
+            .toString(16)
+            .slice(2),
+          method: method,
+          params: params
+        })
+      })
+        .then(response => {
           if (response.ok) {
-            response.json().then((rpcResponse) => {
+            response.json().then(rpcResponse => {
               if ("error" in rpcResponse) {
-                this.help({command : method}).then((({help}) => {
-                  console.log(`${method}`);
-                  console.log(method.replace(/./g, '_'))
-                  console.log(help);
-                  console.error('Error:', rpcResponse.error.message)
-                  reject(rpcResponse.error);
-                })).catch(error => {
-                  reject(rpcResponse.error)
-                })
-              }
-              else {
+                const error = {
+                  message : rpcResponse.error
+                }
+                this.help({ command : method })
+                  .then(({ help }) => {
+                    error.help = help
+                    reject(error);
+                  })
+                  .catch(error => {
+                    reject(error);
+                  });
+              } else {
                 resolve(rpcResponse.result);
               }
             });
-          }
-          else {
+          } else {
             reject({
               code: -1,
               message: "Network error",
@@ -46,14 +54,15 @@ class RPCClient {
             });
           }
         })
-        .catch((error) => {
+        .catch(error => {
+          console.log(error)
           reject({
             code: -1,
             message: "Network error"
           });
         });
     });
-  };
+  }
 
 {{CONTENT}}
 
